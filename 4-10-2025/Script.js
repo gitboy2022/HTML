0f3ec0c4fff2ddf8553b62e12782c3c05862d2b3
Brian Ou <brianou2011m@protonmail.com>
2025-04-17 20:33:41 -0400

    Add files via upload

---
+++ 4-10-2025/Script.js
@@ -0,0 +1,9 @@
+let browser = prompt("What is your browser name?");
+if (browser === "Edge") {
+    alert("You got the Edge browser!");
+} else if (browser==="Chrome" || browser==="Firefox" || browser==="Safari" || browser === "Opera") {
+    alert("The Opera browser is also supported!");
+}
+else {
+    alert("We hope that this page looks ok!");
+}
